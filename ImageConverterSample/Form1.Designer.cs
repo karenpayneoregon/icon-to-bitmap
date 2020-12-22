3dded9792ccda2227b8c54ae6ca06d46bb841986
Karen Payne <Karen.1.payne@oregon.gov>
2020-12-21 19:32:15 -0800

    Add project files.

---
+++ ImageConverterSample/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace ImageConverterSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Agent1PictureBox = new System.Windows.Forms.PictureBox();
+            this.cSharpPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ExcelPictureBox = new System.Windows.Forms.PictureBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SearchPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Agent1PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cSharpPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExcelPictureBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Agent1PictureBox
+            // 
+            this.Agent1PictureBox.Location = new System.Drawing.Point(22, 58);
+            this.Agent1PictureBox.Name = "Agent1PictureBox";
+            this.Agent1PictureBox.Size = new System.Drawing.Size(48, 42);
+            this.Agent1PictureBox.TabIndex = 1;
+            this.Agent1PictureBox.TabStop = false;
+            // 
+            // cSharpPictureBox
+            // 
+            this.cSharpPictureBox.Location = new System.Drawing.Point(101, 57);
+            this.cSharpPictureBox.Name = "cSharpPictureBox";
+            this.cSharpPictureBox.Size = new System.Drawing.Size(48, 42);
+            this.cSharpPictureBox.TabIndex = 2;
+            this.cSharpPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Agent";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "C#";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Excel";
+            // 
+            // ExcelPictureBox
+            // 
+            this.ExcelPictureBox.Location = new System.Drawing.Point(193, 63);
+            this.ExcelPictureBox.Name = "ExcelPictureBox";
+            this.ExcelPictureBox.Size = new System.Drawing.Size(48, 23);
+            this.ExcelPictureBox.TabIndex = 5;
+            this.ExcelPictureBox.TabStop = false;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CloseButton.Location = new System.Drawing.Point(166, 131);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 7;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(266, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.ExitToolStripMenuItem.Text = "Exit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 9;
+            // 
+            // SearchPictureBox
+            // 
+            this.SearchPictureBox.Location = new System.Drawing.Point(128, 137);
+            this.SearchPictureBox.Name = "SearchPictureBox";
+            this.SearchPictureBox.Size = new System.Drawing.Size(18, 18);
+            this.SearchPictureBox.TabIndex = 10;
+            this.SearchPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 170);
+            this.Controls.Add(this.SearchPictureBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ExcelPictureBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cSharpPictureBox);
+            this.Controls.Add(this.Agent1PictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convert icon to bmp";
+            ((System.ComponentModel.ISupportInitialize)(this.Agent1PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cSharpPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExcelPictureBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Agent1PictureBox;
+        private System.Windows.Forms.PictureBox cSharpPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox ExcelPictureBox;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox SearchPictureBox;
+    }
+}
+
---
+++ ImageConverterSample/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageConverterSample.LanguageExtensions;
+using ImageHelpers;
+
+namespace ImageConverterSample
+{
+    
+#nullable enable
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Shown += OnShown;
+        }
+
+        private void OnShown(object? sender, EventArgs e)
+        {
+
+            var basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Icons");
+            
+            Agent1PictureBox.LoadIconFromFile(Path.Combine(basePath, "agent1.ico"));
+            cSharpPictureBox.LoadIconFromFile(Path.Combine(basePath, "Csharp.ico"));
+            ExcelPictureBox.LoadIconFromFile(Path.Combine(basePath, "Excel.ico"));
+            SearchPictureBox.Image = Converters.FromBytesToBitMap(Path.Combine(basePath, "Search.ico"));
+
+            var closeImage = Converters.FromBytesToBitMap(Path.Combine(basePath, "Close.ico"));
+            CloseButton.Image = closeImage;
+            ExitToolStripMenuItem.Image = closeImage;
+
+        }
+        
+    }
+}
